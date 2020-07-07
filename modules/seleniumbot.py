12de2e3833ac6cf3ac756a3d154bc60759a1556c
mehrdad0mms <mehrdad0mms@gmail.com>
2020-07-07 16:54:32 +0430

    PEP8 formatting

--- modules/seleniumbot.py
+++ modules/seleniumbot.py
@@ -22,15 +22,16 @@ import logging
 
 # from pymailutils import Imap
 
-class AccountCreator():
+
+class AccountCreator:
     account_created = 0
+
     def __init__(self, use_custom_proxy, use_local_ip_address):
         self.sockets = []
         self.use_custom_proxy = use_custom_proxy
         self.use_local_ip_address = use_local_ip_address
         self.url = 'https://www.instagram.com/'
         self.__collect_sockets()
-
 
     def __collect_sockets(self):
         r = requests.get("https://www.sslproxies.org/")
@@ -39,9 +40,9 @@ class AccountCreator():
         for socket_str in revised_list:
             self.sockets.append(socket_str[:-5].replace("</td>", ":"))
 
-    def createaccount(self, proxy=None):
+    def create_account(self, proxy=None):
         chrome_options = webdriver.ChromeOptions()
-        if proxy != None:
+        if proxy is None:
             chrome_options.add_argument('--proxy-server=%s' % proxy)
 
         # chrome_options.add_argument('headless')
@@ -56,9 +57,6 @@ class AccountCreator():
 
         print('Browser Opened')
         sleep(5)
-
-
-
 
         action_chains = ActionChains(driver)
         sleep(5)
@@ -116,14 +114,12 @@ class AccountCreator():
             age_button = driver.find_element_by_xpath( "//input[@name='ageRadio' and @value='above_18']")
             age_button.click()
 
-
             sleep(2)
             next_button = driver.find_elements_by_xpath('//button[text()="Next"]')[1]
             next_button.click()
 
-        except Exception as e :
+        except Exception as e:
             pass
-
 
         sleep(4)
         # After the first fill save the account account_info
@@ -141,16 +137,16 @@ class AccountCreator():
 
     def creation_config(self):
         try:
-            if self.use_local_ip_address == False:
-                if self.use_custom_proxy == False:
+            if self.use_local_ip_address is False:
+                if self.use_custom_proxy is False:
                     for i in range(0, config.Config['amount_of_account']):
                         if len(self.sockets) > 0:
                             current_socket = self.sockets.pop(0)
                             try:
-                                self.createaccount(current_socket)
+                                self.create_account(current_socket)
                             except Exception as e:
                                 print('Error!, Trying another Proxy {}'.format(current_socket))
-                                self.createaccount(current_socket)
+                                self.create_account(current_socket)
 
                 else:
                     with open(config.Config['proxy_file_path'], 'r') as file:
@@ -160,9 +156,9 @@ class AccountCreator():
 
                             if amount_per_proxy != 0:
                                 print("Creating {} amount of users for this proxy".format(amount_per_proxy))
-                                for i in range(0, amount_per_proxy):
+                                for _ in range(0, amount_per_proxy):
                                     try:
-                                        self.createaccount(proxy)
+                                        self.create_account(proxy)
 
                                     except Exception as e:
                                         print("An error has occured" + e)
@@ -170,20 +166,18 @@ class AccountCreator():
                             else:
                                 random_number = randint(1, 20)
                                 print("Creating {} amount of users for this proxy".format(random_number))
-                                for i in range(0, random_number):
+                                for _ in range(0, random_number):
                                     try:
-                                        self.createaccount(proxy)
+                                        self.create_account(proxy)
                                     except Exception as e:
                                         print(e)
             else:
-                for i in range(0, config.Config['amount_of_account']):
-                            try:
-                                self.createaccount()
-                            except Exception as e:
-                                print('Error!, Check its possible your ip might be banned')
-                                self.createaccount()
-
-
+                for _ in range(0, config.Config['amount_of_account']):
+                    try:
+                        self.create_account()
+                    except Exception as e:
+                        print('Error!, Check its possible your ip might be banned')
+                        self.create_account()
         except Exception as e:
             print(e)
 
